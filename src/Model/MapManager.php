119e0da4e564f17fa189ed59264d0c67b91c8c27
Damien Rambourg <damien.rambourg@gmail.com>
2019-04-18 14:32:03 +0200

    Création du controlleur map et manager

---
+++ src/Model/MapManager.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class MapManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'map';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+
+    /**
+     * @param array $item
+     * @return int
+     */
+    public function insert(array $map): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO $this->table (`title`) VALUES (:title)");
+        $statement->bindValue('title', $map['title'], \PDO::PARAM_STR);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
+
+
+    /**
+     * @param int $id
+     */
+    public function delete(int $id): void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+
+    /**
+     * @param array $item
+     * @return bool
+     */
+    public function update(array $map):bool
+    {
+
+        // prepared request
+        $statement = $this->pdo->prepare("UPDATE $this->table SET `title` = :title WHERE id=:id");
+        $statement->bindValue('id', $map['id'], \PDO::PARAM_INT);
+        $statement->bindValue('title', $map['title'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+}
