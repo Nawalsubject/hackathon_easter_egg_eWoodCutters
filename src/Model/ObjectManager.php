a5e1f1fb03d20e2b33e2692f3697fca215cfd9e2
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-04-18 18:17:58 +0200

    Clean index

--- src/Model/ObjectManager.php
+++ src/Model/ObjectManager.php
@@ -30,49 +30,35 @@ class ObjectManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-    /**
-     * Récuperer nombre d'oeufs
-     *
-     * @param $player_id
-     * @return int|void
-     */
-    public function getCountEgg($player_id)
+
+    public function getCountEgg(int $player_id)
     {
-        $statement = $this->pdo->prepare("SELECT id as nbEgg FROM $this->table WHERE content_type_id=:egg AND player_id=$player_id");
+        $statement = $this->pdo->prepare("SELECT id as nbEgg FROM $this->table 
+        WHERE content_type_id=:egg AND player_id=$player_id");
         $statement->bindValue('egg', self::EGG);
 
         $statement->execute();
         return count($statement->fetchAll());
     }
 
-    /**
-     * Récuperer le nombre de lait
-     *
-     * @param $player_id
-     * @return int|void
-     */
-    public function getCountMilk($player_id)
+    public function getCountMilk(int $player_id)
     {
-        $statement = $this->pdo->prepare("SELECT id as nbMilk FROM $this->table WHERE content_type_id=:milk AND player_id=$player_id");
+        $statement = $this->pdo->prepare("SELECT id as nbMilk FROM $this->table
+        WHERE content_type_id=:milk AND player_id=$player_id");
         $statement->bindValue('milk', self::MILK);
 
         $statement->execute();
         return count($statement->fetchAll());
     }
 
-    /**
-     * Récuperer le nombre de chocolats
-     *
-     * @param $player_id
-     * @return int|void
-     */
-    public function getCountChocolate($player_id)
+
+    public function getCountChocolate(int $player_id)
     {
-        $statement = $this->pdo->prepare("SELECT id as nbChocolate FROM $this->table WHERE content_type_id=:chocolate AND player_id=$player_id");
+        $statement = $this->pdo->prepare("SELECT id as nbChocolate FROM $this->table 
+        WHERE content_type_id=:chocolate AND player_id=$player_id");
         $statement->bindValue('chocolate', self::CHOCOLATE);
 
         $statement->execute();
-        return  count($statement->fetchAll());
+        return count($statement->fetchAll());
     }
-
 }
