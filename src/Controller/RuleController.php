9a225f1659a8ba1c817eecb3a8476557c3fe741f
Vang Jeannette <vangjeannette@gmail.com>
2019-04-18 16:22:33 +0200

    création de la page règles du jeu non finalisée

---
+++ src/Controller/RuleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+class RuleController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Rules/index.html.twig');
+    }
+}
