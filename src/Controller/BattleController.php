317b2fd6fc52f44cad5c3f7fd24a8cd4be147302
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-04-19 13:58:32 +0200

    HEAVY METAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL

--- src/Controller/BattleController.php
+++ src/Controller/BattleController.php
@@ -32,33 +32,8 @@ class BattleController extends AbstractController
      */
     public function index()
     {
-
         $playerManager = new PlayerManager;
-        $players = $playerManager->selectAll();
-
-        $objectManager = new ObjectManager();
-        $bag1= $objectManager->getBag(1);
-        $bag2= $objectManager->getBag(2);
-        $milkPlayer1 = $objectManager->getCountMilk(1);
-        $milkPlayer2 = $objectManager->getCountMilk(2);
-        $chocolatePlayer1 = $objectManager->getCountChocolate(1);
-        $chocolatePlayer2 = $objectManager->getCountChocolate(2);
-        $eggPlayer1 = $objectManager->getCountEgg(1);
-        $eggPlayer2 = $objectManager->getCountEgg(2);
-
-
-
-        $map = new Map(12, 12, 3, 6, 4, 3);
-
-        $mapCells = $map->getAllCells();
-
-        return $this->twig->render('battle/index.html.twig', [
-            'map' => $map,
-            'cells' => $mapCells,
-            'players' => $players,
-            'player_id'=> 2,
-        'player1'=> ['milk' => $milkPlayer1, 'chocolate' => $chocolatePlayer1, 'egg' => $eggPlayer1, 'bag' => $bag1],
-        'player2'=> ['milk' => $milkPlayer2, 'chocolate' => $chocolatePlayer2, 'egg' => $eggPlayer2, 'bag' => $bag2]
-        ]);
+        $player = $playerManager->selectAll();
+        return $this->twig->render('Battle/index.html.twig', ['players'=> $player]);
     }
 }
