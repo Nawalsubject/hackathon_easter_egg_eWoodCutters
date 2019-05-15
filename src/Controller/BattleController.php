a7c0b0c1008ac5a0d317eb1009187e4fa9c14056
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-05-15 15:45:45 +0200

    Fix battle cards

--- src/Controller/BattleController.php
+++ src/Controller/BattleController.php
@@ -32,8 +32,43 @@ class BattleController extends AbstractController
      */
     public function index()
     {
+        $playerNext = 1;
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['player_next'])) {
+                $playerNext = $_POST['player_next'];
+                if ($playerNext == 2) {
+                    $playerNext = 1;
+                } else {
+                    $playerNext = 2;
+                }
+            };
+        }
+
         $playerManager = new PlayerManager;
-        $player = $playerManager->selectAll();
-        return $this->twig->render('Battle/index.html.twig', ['players'=> $player]);
+        $players = $playerManager->selectAll();
+
+        $objectManager = new ObjectManager();
+        $milkPlayer1 = $objectManager->getCountMilk(1);
+        $milkPlayer2 = $objectManager->getCountMilk(2);
+        $chocolatePlayer1 = $objectManager->getCountChocolate(1);
+        $chocolatePlayer2 = $objectManager->getCountChocolate(2);
+        $eggPlayer1 = $objectManager->getCountEgg(1);
+        $eggPlayer2 = $objectManager->getCountEgg(2);
+
+
+        $map = new Map(12, 12, 3, 6, 4, 3);
+
+        $mapCells = $map->getAllCells();
+
+
+        return $this->twig->render('Battle/index.html.twig', [
+            'map' => $map,
+            'cells' => $mapCells,
+            'players' => $players,
+            'player_id' => $playerNext,
+            'player1' => ['milk' => $milkPlayer1, 'chocolate' => $chocolatePlayer1, 'egg' => $eggPlayer1],
+            'player2' => ['milk' => $milkPlayer2, 'chocolate' => $chocolatePlayer2, 'egg' => $eggPlayer2],
+            'content' => false,
+        ]);
     }
 }
